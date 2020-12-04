d61e75d8eb24fb6cc1e22abb3a2d2a522493a73e
Danial <danial.eshghi@gmail.com>
2020-12-04 13:40:47 +0330

    Django Rest Framework License Used Count Increments

--- account/middlewares.py
+++ account/middlewares.py
@@ -7,7 +7,7 @@ class HitCalculatorMiddleware:
 	def __call__(self, request):
 		response = self.get_response(request)
 
-		if request.path.startswith('/api/'):
+		if request.path.startswith('/api/') and 200 <= response.status_code < 300:
 			self.license_increment(request, 1)
 
 		return response
